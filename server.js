271b5c49740de559a0b0b82c1e5dfa5a7e313102
wilson <lockleeliumiaowilson@gmail.com>
2022-04-04 20:47:30 +1000

    add token check

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const parseLog = require('./logParser');
 const app = express();
 const port = 8000;
+
+const _token = fs.readFileSync('token', 'UTF-8');
 
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
@@ -19,6 +22,14 @@ app.get('/version', async (req, res) => {
 app.post('/automation', async (req, res) => {
     const code = req.body.code;
     const data = req.body.data;
+    const token = req.body.token;
+
+    if(_token !== token) {
+        return res.json({
+            success: false,
+            message: 'Invalid token',
+        });
+    }
 
     let $data = {};
     try {
